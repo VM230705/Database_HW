cb4103d991e3466b8d031a9f269018b5787ce523
lyz508 <thomas7892009@gmail.com>
2022-05-12 03:20:47 +0800

    Add db_account information for connection, Finished Registeration form check

--- php_ajax/check_name.php
+++ php_ajax/check_name.php
@@ -1,9 +1,4 @@
 <?php
-$dbservername='localhost';
-$dbname='db_hw';
-$dbusername='root';
-$dbpassword='DBHW2';
-
 try {
     if (!isset($_REQUEST['uname']) || empty($_REQUEST['uname']))
     {
@@ -12,24 +7,20 @@ try {
     }
 
     $uname=$_REQUEST['uname'];
-    $conn = new PDO("mysql:host=$dbservername;dbname=$dbname", 
-        $dbusername, $dbpassword);
+    $conn = require_once "../db_account/config.php";
 
-    // set the PDO error mode to exception
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     // query
     $stmt=$conn->prepare("select name from user where name=:username");
     $stmt->execute(array('username' => $uname));
 
-    if ($stmt->rowCount()==0){
+    if ($stmt->rowCount() == 0){
         echo 'YES';
     }
     else {
         echo 'NO';
     }
 }
-catch(Exception $e)
-{ 
+catch(Exception $e){ 
     echo $e->getMessage();
 }
 ?>
