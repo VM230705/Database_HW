fb4b9f41f1aa71c4f6ed9fb23dc06100af0f872b
lyz508 <thomas7892009@gmail.com>
2022-05-12 03:24:21 +0800

    Fixed merge problem

--- php_ajax/check_name.php
+++ php_ajax/check_name.php
@@ -1,12 +1,4 @@
 <?php
-<<<<<<< HEAD
-=======
-$dbservername='localhost';
-$dbname='db_hw';
-$dbusername='root';
-$dbpassword='DBHW2';
-
->>>>>>> 6ab4323ddccbe1cd77031afa325e6e8da93f6a88
 try {
     if (!isset($_REQUEST['uname']) || empty($_REQUEST['uname']))
     {
@@ -15,37 +7,20 @@ try {
     }
 
     $uname=$_REQUEST['uname'];
-<<<<<<< HEAD
     $conn = require_once "../db_account/config.php";
 
-=======
-    $conn = new PDO("mysql:host=$dbservername;dbname=$dbname", 
-        $dbusername, $dbpassword);
-
-    // set the PDO error mode to exception
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
->>>>>>> 6ab4323ddccbe1cd77031afa325e6e8da93f6a88
     // query
     $stmt=$conn->prepare("select name from user where name=:username");
     $stmt->execute(array('username' => $uname));
 
-<<<<<<< HEAD
     if ($stmt->rowCount() == 0){
-=======
-    if ($stmt->rowCount()==0){
->>>>>>> 6ab4323ddccbe1cd77031afa325e6e8da93f6a88
         echo 'YES';
     }
     else {
         echo 'NO';
     }
 }
-<<<<<<< HEAD
 catch(Exception $e){ 
-=======
-catch(Exception $e)
-{ 
->>>>>>> 6ab4323ddccbe1cd77031afa325e6e8da93f6a88
     echo $e->getMessage();
 }
 ?>
