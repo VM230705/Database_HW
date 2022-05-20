5fd9a3a1946836ba844cd3bbfa5a69eb3d0c3fab
linyz <linyz@cs.nctu.edu.tw>
2022-05-20 22:36:44 +0800

    Merge up group work & add format check on edit location

--- scripts/renew_location.js
+++ scripts/renew_location.js
@@ -2,6 +2,11 @@ function renew_location (){
     let latitude = document.getElementById("latitude").value,
         longitude = document.getElementById("longitude").value;
 
+    if (/^[0-9]{1,}.[0-9]{1,}$/.test(latitude) == false || /^[0-9]{1,}.[0-9]{1,}$/.test(longitude) == false ){
+        alert ('Wrong format ! Must be float numbers !!');
+        return false;
+    }
+
     let xhr = new XMLHttpRequest();
     xhr.onreadystatechange = function (){
         // get response
